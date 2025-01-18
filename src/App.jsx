8c3e39a35259635ce817fd84f6090a87a4afd798
GmMamunH <gmmamun3004@gmail.com>
2025-01-18 21:26:00 +0600

    add(navbar): add navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
+import { Navbar } from "./components/shared/header/navbar/Navbar";
+
 function App() {
   return (
     <>
       <h1 className="text-center text-3xl text-gray-950 font-bold">
         IQA: ইসলামী প্রশ্নোত্তর
       </h1>
+      <Navbar />
     </>
   );
 }
